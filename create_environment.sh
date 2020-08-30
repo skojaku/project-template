d311f2b3ea1b64197fae52cacc920bf85af3978c
SADAMORI KOJAKU <freesailing4046@gmail.com>
2020-08-30 07:17:56 -0400

    Update create_environment.sh

--- create_environment.sh
+++ create_environment.sh
@@ -1,2 +1,2 @@
 #!/bin/zsh
-conda create -n $1 -c bioconda -c conda-forge python=3.7 ipykernel snakemake flake8 pyflakes pep8 pylint jedi
+conda create -n $1 -c bioconda -c conda-forge python=3.7 ipykernel snakemake snakefmt flake8 pyflakes pep8 pylint jedi
